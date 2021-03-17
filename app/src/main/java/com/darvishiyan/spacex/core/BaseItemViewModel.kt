0d76bcebe32a0ad88f00a143848a261cf1372c01
Hesam Darvishiyan <hesam.darvishiyan@snapp.cab>
2021-03-17 10:50:52 +0330

    add adapter base classes

---
+++ app/src/main/java/com/darvishiyan/spacex/core/BaseItemViewModel.kt
@@ -0,0 +1,56 @@
+package com.darvishiyan.spacex.core
+
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.Observable
+import androidx.databinding.PropertyChangeRegistry
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+import org.koin.core.KoinComponent
+
+abstract class BaseItemViewModel<DB : ViewDataBinding> :
+    ViewModel(),
+    Observable,
+    KoinComponent,
+    LifecycleObserver {
+
+    private val callbacks: PropertyChangeRegistry = PropertyChangeRegistry()
+    lateinit var binding: DB
+        private set
+
+    //<editor-fold desc="Observable">
+    override fun addOnPropertyChangedCallback(
+        callback: Observable.OnPropertyChangedCallback
+    ) {
+        callbacks.add(callback)
+    }
+
+    override fun removeOnPropertyChangedCallback(
+        callback: Observable.OnPropertyChangedCallback
+    ) {
+        callbacks.remove(callback)
+    }
+
+    fun notifyChange() {
+        callbacks.notifyCallbacks(this, 0, null)
+    }
+
+    fun notifyPropertyChanged(fieldId: Int) {
+        callbacks.notifyCallbacks(this, fieldId, null)
+    }
+    //</editor-fold>
+
+    /**
+     * call this method after bind layout to [DB] in onCreateView
+     */
+    open fun onBindView(binding: DB) {
+        this.binding = binding
+    }
+
+    /**
+     * call this method in onViewCreated
+     */
+    open fun onViewBounded(view: View, savedInstanceState: Bundle?) {}
+
+}
---
+++ app/src/main/java/com/darvishiyan/spacex/core/BaseRecyclerViewAdapter.kt
@@ -0,0 +1,84 @@
+package com.darvishiyan.spacex.core
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.recyclerview.widget.RecyclerView
+import org.koin.core.KoinComponent
+
+abstract class BaseRecyclerViewAdapter<T> :
+    RecyclerView.Adapter<BaseRecyclerViewAdapter<T>.ItemViewHolder>(), KoinComponent {
+
+    abstract val itemLayoutResId: Int
+    abstract val viewModelId: Int
+    var items = listOf<T>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ViewDataBinding = onBindView(layoutInflater, parent, viewType)
+        return ItemViewHolder(binding)
+    }
+
+    open fun onBindView(
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewDataBinding =
+        DataBindingUtil.inflate(layoutInflater, itemLayoutResId, parent, false)
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) =
+        holder.bind(getViewModel(position, items[position]))
+
+    abstract fun getViewModel(position: Int, item: T): BaseItemViewModel<*>
+
+    override fun onViewAttachedToWindow(holder: ItemViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        holder.onAppear()
+    }
+
+    override fun onViewDetachedFromWindow(holder: ItemViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.onDisappear()
+    }
+
+    inner class ItemViewHolder(
+        private val binding: ViewDataBinding
+    ) : RecyclerView.ViewHolder(binding.root), LifecycleOwner {
+
+        private val lifecycleRegistry = LifecycleRegistry(this)
+
+        init {
+            lifecycleRegistry.currentState = Lifecycle.State.INITIALIZED
+        }
+
+
+        fun onAppear() {
+            lifecycleRegistry.currentState = Lifecycle.State.CREATED
+            lifecycleRegistry.currentState = Lifecycle.State.STARTED
+        }
+
+        fun onDisappear() {
+            lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+        }
+
+        override fun getLifecycle() = lifecycleRegistry
+
+        fun bind(itemViewModel: BaseItemViewModel<*>) {
+            binding.apply {
+                lifecycleOwner = this@ItemViewHolder
+                setVariable(viewModelId, itemViewModel)
+                executePendingBindings()
+            }
+        }
+    }
+}
